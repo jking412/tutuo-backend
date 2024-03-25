023f5dd17e8d34fa1d223439733da4998d44f0f7
jking412 <3250532948@qq.com>
2024-03-25 17:04:33 +0800

    feat: new first commit

--- src/main/java/com/wjk/tutuo1/Tutuo1Application.java
+++ src/main/java/com/wjk/tutuo1/Tutuo1Application.java
@@ -14,31 +14,31 @@ public class Tutuo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Tutuo1Application.class, args);
     }
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-
-        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
-
-        return tomcat;
-
-    }
-
-    private Connector createHTTPConnector() {
-
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-
-        //同时启用http（8080）、https（443）两个端口
-        connector.setScheme("http");
-
-        connector.setSecure(false);
-
-        connector.setPort(8080);
-
-        connector.setRedirectPort(443);
-
-        return connector;
-
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//
+//        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+//
+//        return tomcat;
+//
+//    }
+//
+//    private Connector createHTTPConnector() {
+//
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//
+//        //同时启用http（8080）、https（443）两个端口
+//        connector.setScheme("http");
+//
+//        connector.setSecure(false);
+//
+//        connector.setPort(8080);
+//
+//        connector.setRedirectPort(443);
+//
+//        return connector;
+//
+//    }
 }
